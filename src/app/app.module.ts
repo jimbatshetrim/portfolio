425586e6a0a29b495ba0e1796135346bff1a3774
jimba tshetrim <jtshetrim@gmail.com>
2023-04-15 17:58:06 +0600

    feat: skeleton styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ProfileComponent,
+    MainComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
